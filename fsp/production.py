e94a7ccbcef74d00d8090cf6d2002a8087ecc1c5
voffan <vv.everstov@s-vfu.ru>
2024-12-06 08:47:06 +0900

    prod settings added

---
+++ fsp/production.py
@@ -0,0 +1,14 @@
+import settings
+
+settings.DEBUG = False
+
+settings.DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prod',
+        'HOST': '213.171.9.104',
+        'PORT': 3306,
+        'USER': 'gen_user',
+        'PASSWORD': 'hHZFlUgO$.M1hs',
+    }
+}
