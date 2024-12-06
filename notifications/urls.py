f73ad8a0fb5b3fde56ecf951a43b5a5692bb85c5
voffan <vv.everstov@s-vfu.ru>
2024-12-06 09:43:46 +0900

    added notifications

---
+++ notifications/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import NotificationView
+
+urlpatterns = [
+    path('/', NotificationView.as_view({'get': 'list'})),
+    path('/<int:notification_id>/', NotificationView.as_view({'delete': 'delete'})),
+]
