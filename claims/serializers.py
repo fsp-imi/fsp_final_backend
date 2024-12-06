8c81ac65428002e3a768ed240541e93f07b84831
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-12-07 02:06:59 +0900

    updated claims

--- claims/serializers.py
+++ claims/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Claim
+from .models import ClaimFile, Claim
+
+class ClaimFileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ClaimFile
+        fields = ['id', 'claim', 'file', 'description']
 
 class ClaimSerializer(serializers.ModelSerializer):
     class Meta:
         model = Claim
-        fields = ['id', ]
+        fields = ['id', 'name', 'start_time', 'end_time', 'place', 'format', 'status']
