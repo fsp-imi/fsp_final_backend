995c62f2573186cb282371623469bb822209428f
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-12-06 16:27:59 +0900

    added /users/<id>

--- users/views.py
+++ users/views.py
@@ -22,6 +22,12 @@ class UserViewSet(ModelViewSet):
         user = serializer.save()
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key}, status=HTTP_201_CREATED)
+    
+    def retrieve(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = get_object_or_404(User, pk=user_id)
+        serializer = self.get_serializer(user)
+        return Response(serializer.data, status=HTTP_200_OK)
 
 
 class UserProfile(APIView):
