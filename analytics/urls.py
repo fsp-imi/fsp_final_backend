ba0c179e3308971dc29fd15e05d05894597a3708
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-12-07 22:34:26 +0900

    2234

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import SportTypeView, DisciplineView, ContestTypeView, AgeGroupView,ContestView, ContestDisciplineView, ContestAgeGroupView
 
 urlpatterns = [
-    path('', SportTypeView.as_view({'get': 'list'})),
+    path('result/filter/', SportTypeView.as_view({'get': 'filter'})),
 ]
