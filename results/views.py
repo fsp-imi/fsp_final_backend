d934188a153672a8d9a09bb5b84a4d0b71a145aa
voffan <vv.everstov@s-vfu.ru>
2024-12-07 19:11:14 +0900

    revert

--- results/views.py
+++ results/views.py
@@ -15,17 +15,3 @@ class ResultView(ModelViewSet):
 
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
-
-
-class FileUploadView(APIView):
-    parser_classes = [FileUploadParser]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, format=None):
-        dir = '/upload/'
-        for filename, file_obj in request.FILES.items():
-            with open(dir + filename, 'wb') as f:
-                f.write(file_obj.read())
-            parse_file(dir + filename)
-        # do some stuff with uploaded file
-        return Response(status=204)
