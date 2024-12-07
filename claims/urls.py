756f89e21904cdb90c4e96cba5a77a278bd00943
Vladislav Argunov <90658819+kawasaki4112@users.noreply.github.com>
2024-12-07 17:36:27 +0900

    DFGHYJTUKJTHRGEF

--- claims/urls.py
+++ claims/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ClaimView
 
 urlpatterns = [
-    path('', ClaimView.as_view({'get': 'list', 'post':'post'})),
+    path('', ClaimView.as_view({'get': 'list', 'post':'create'})),
     path('<int:pk>/', ClaimView.as_view({'get': 'get_by_id', 
                                          'put': 'put', 
                                          'post': 'post'})),
