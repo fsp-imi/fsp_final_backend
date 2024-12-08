71d2921ec9f188b47f45304e4f3e3f961f5cfd49
voffan <vv.everstov@s-vfu.ru>
2024-12-08 10:54:07 +0900

    Some fixes on Notifications

--- notifications/NotificationsFuncs.py
+++ notifications/NotificationsFuncs.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 def sendNotification(contest):
     from subscription.models import Subscription
     #users = Subscription.objects.filter(contest__id=contest.id).values_list('user__id', flat=True)
-    users = User.objects.all()
+    users = User.objects.filter(is_staff=False)
     for user in User.objects.filter(id__in=users):
-        Notification.objects.create(user=user, text=f'Обратите внимание! Информация по соревнованию \"{contest.program}\" изменилась!')
+        Notification.objects.create(user=user, text=f'Обратите внимание! Информация по соревнованию \"{contest.name}\" изменилась!')
     
 def sendClaimNotification(claim, text, user):
     Notification.objects.create(user=user, text=text)
