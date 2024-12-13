a81bc6eeabbe21f5f7eca3df00f0747f96501b15
voffan <>
2024-12-13 20:55:44 +0900

    password reset

--- fsp/urls.py
+++ fsp/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetCompleteView, PasswordResetConfirmView
+#from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetCompleteView, PasswordResetConfirmView
 
 from s3.views import UploadFileView
 
@@ -38,8 +38,9 @@ apiv1 = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include((apiv1, 'fsp'), namespace='apiv1')),
-    path('password_reset', PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
-    path('password_reset/done', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/complete', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'), 
+    #path('password_reset', PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    #path('password_reset/done', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    #path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    #path('reset/complete', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'), 
+    path('password/reset/', include('django_rest_passwordreset.urls')),
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+### api/users/models.py
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django_rest_passwordreset.signals import reset_password_token_created
+...
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    context = {
+        'email': reset_password_token.user.email,
+        'reset_password_url': f"http://localhost:3000/password/reset/confirm/?token={reset_password_token.key}"
+    }
+    #email_html_message = render_to_string('email/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+    msg = EmailMultiAlternatives("Password Reset", email_plaintext_message, [], [reset_password_token.user.email])
+    #msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
